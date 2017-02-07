c95e41fab6aca00d789bc01c5844c0563107037c
Robert Balaban <rob.balaban@gmail.com>
2017-02-07 23:33:54 +0200

    Completed encrypt / decrypt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-# from numpy.linalg import inv
+from numpy.linalg import inv
 
 # Get the users Input
 
@@ -10,8 +10,6 @@ for character in input:
   number = ord(character) - 96
   originalMessage.append(number)
 
-print originalMessage
-
 input = raw_input('Establish your decryption Key:')
 input = input.lower()
 encryptKey = []
@@ -19,36 +17,45 @@ for elements in  input:
 	intermediate = ord(elements) - 96
 	encryptKey.append(intermediate)
 
-print encryptKey
-
 # Get the length of input
 inputLen = len(originalMessage)
 
 ceilInt = np.int(np.ceil(np.sqrt(inputLen)))**2
-print ceilInt
 
 # Modulo, this is our n in nXn
 modHelper = np.sqrt(ceilInt)
 
 rows, cols = np.int(modHelper), np.int(modHelper)
-matrix = np.zeros(shape=(rows, cols))
+messageEncrypt = np.zeros(shape=(rows, cols))
 encryptMatrix = np.zeros(shape=(rows, cols)) 
 
 # Append our values in our created matrix
 for (i, v) in zip(range(0, len(originalMessage)), originalMessage):
 	r, c = i / cols, i % cols
-	matrix[r, c] = v
-	encryptMatrix[r, c] = v
+	messageEncrypt[r, c] = v
 
 # Create our encryptionKey Matrix
 for (i, v) in zip(range(0, len(encryptKey)), encryptKey):
 	r, c = i / cols, i % cols
 	encryptMatrix[r, c] = v
-# Multiply our message by our encrypt key
-
 
 # Calculate the inverse matrix for our decrypt key
 print 'Our initial matrix is:'
-print matrix
+print messageEncrypt
 print 'Our encrypt matrix is:'
 print encryptMatrix
+
+# Multiply our message by our encrypt key
+encryptedMessage = np.dot(messageEncrypt, encryptMatrix)
+print 'Our encrypted message is:'
+print encryptedMessage
+
+# Inverse the encryption key for awesomeness
+decryptionKey = inv(np.matrix(encryptMatrix))
+print 'Our decryption key is:'
+print decryptionKey
+
+# Our decrypted matrix is
+decryptedMessage = np.dot(encryptedMessage, decryptionKey)
+print 'Our decrypted message is:'
+print decryptedMessage
