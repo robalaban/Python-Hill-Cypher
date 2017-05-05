74d79bd1ad3dfe5d112b701e6caa94b08a98e47e
Robert Balaban <rob.balaban@gmail.com>
2017-05-05 22:47:07 +0300

    Added Functiones DRYed the project

--- main.py
+++ main.py
@@ -1,54 +1,52 @@
 import numpy as np
-# from numpy.linalg import inv
+from numpy.linalg import inv
 
-# Get the users Input
-
-input = raw_input('Message to Encrypt:')
-input = input.lower()
 originalMessage = []
-for character in input:
-  number = ord(character) - 96
-  originalMessage.append(number)
-
-print originalMessage
-
-input = raw_input('Establish your decryption Key:')
-input = input.lower()
 encryptKey = []
-for elements in  input:
-	intermediate = ord(elements) - 96
-	encryptKey.append(intermediate)
 
-print encryptKey
+
+def get_user_input(value, msg):
+    input = raw_input(msg)
+    input = input.lower()
+    for character in input:
+        number = ord(character) - 96
+        value.append(number)
+
+get_user_input(originalMessage, 'Enter Message to Encrypt')
+get_user_input(encryptKey, 'Enter decryption Key')
 
 # Get the length of input
 inputLen = len(originalMessage)
-
 ceilInt = np.int(np.ceil(np.sqrt(inputLen)))**2
-print ceilInt
 
 # Modulo, this is our n in nXn
 modHelper = np.sqrt(ceilInt)
-
 rows, cols = np.int(modHelper), np.int(modHelper)
-matrix = np.zeros(shape=(rows, cols))
-encryptMatrix = np.zeros(shape=(rows, cols)) 
 
-# Append our values in our created matrix
-for (i, v) in zip(range(0, len(originalMessage)), originalMessage):
-	r, c = i / cols, i % cols
-	matrix[r, c] = v
-	encryptMatrix[r, c] = v
 
-# Create our encryptionKey Matrix
-for (i, v) in zip(range(0, len(encryptKey)), encryptKey):
-	r, c = i / cols, i % cols
-	encryptMatrix[r, c] = v
+# Create our empty matrix and fill it with our values
+def build_matrix(matrix):
+    # Create matrix
+    value = np.zeros(shape=(rows, cols))
+
+    # Append our values
+    for (i, v) in zip(range(0, len(matrix)), matrix):
+        r, c = i / cols, i % cols
+        value[r, c] = v
+
+    return value
+
+originalMessage = build_matrix(originalMessage)
+encryptKey = build_matrix(encryptKey)
+
 # Multiply our message by our encrypt key
+encryptedMessage = np.dot(originalMessage, encryptKey)
+print(encryptedMessage)
 
+# Inverse the encryption key for awesomeness
+decryptionKey = inv(np.matrix(encryptKey))
+print(decryptionKey)
 
-# Calculate the inverse matrix for our decrypt key
-print 'Our initial matrix is:'
-print matrix
-print 'Our encrypt matrix is:'
-print encryptMatrix
+# Our decrypted matrix is
+decryptedMessage = np.dot(originalMessage, decryptionKey)
+print(decryptedMessage)
